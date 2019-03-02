bc3109b80241db1bd76e86ec036c5c85626b7f60
xtrapp <jefersontoga94@gmail.com>
2019-03-01 22:47:39 -0300

    .

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,23 @@
+var a = 3
+let b = 4
+
+var a = 30
+b = 40
+console.log(a,b)
+
+a = 300
+b = 300
+
+console.log(a,b)
+
+const c = 5
+
+console.log(c)
+
+
+/*Existe diferença entre var e let, se possivél
+usar mais o let que o var */
+/*Definir bons nomes para as variavies*/
+/*Com var pode se redaclar uma variavel ja declarada, já o let não*/
+//Const(Constante) não é possivel atribuir um novo valor a esse tive de variavel
+
