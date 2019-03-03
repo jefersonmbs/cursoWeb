02caea0830d8a35a916419312f98ead5426c260d
xtrapp <jefersontoga94@gmail.com>
2019-03-03 11:45:12 -0300

    .

--- exercicios-web/fundamentos/tipagemFraca.js
+++ exercicios-web/fundamentos/tipagemFraca.js
@@ -9,3 +9,10 @@ console.log(typeof qualquer)
 //Js é Fracamente Tipada
 //Desvantagens , mais dificil de encontrar BUG's
 //Vantagem Mais flexivél
+
+
+// Evitar nomes genéricos e siglas
+//Exemplos de declarações
+let valor = '' //Que valor?
+let numero = 1 //Que numero ?
+let pqp = false // Produto Quimico Perigoso
