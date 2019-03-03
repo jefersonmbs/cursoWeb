8b9aee175ebe261a5f5ec937646b680ca30bef5d
xtrapp <jefersontoga94@gmail.com>
2019-03-03 10:40:36 -0300

    .

---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.15
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Js é Fracamente Tipada
+//Desvantagens , mais dificil de encontrar BUG's
