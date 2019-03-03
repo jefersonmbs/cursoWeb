bb0aeeb1c4ff26b188d5f9e2dada5c771a52dbce
xtrapp <jefersontoga94@gmail.com>
2019-03-03 14:17:03 -0300

    Parte de Fundamentos de JS Numeros

--- exercicios-web/fundamentos/numerosAlgunsCuidados.js
+++ exercicios-web/fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,6 @@
 console.log(7 / 0) //Retora INfinito
 console.log("10" / 2) //Converte String em numero e faz a operação
+console.log('3' + 2) // Concatena a String 3 com o numero 2 formando um 32
 console.log("Show!" * 2)
 console.log(0.1 + 0.7);
 console.log((10).toString())
