fd7f97dc53ce3524e6644676ac143ced6f9e1dfd
xtrapp <jefersontoga94@gmail.com>
2019-03-03 13:52:38 -0300

    Parte de Fundamentos de JS Math

---
+++ exercicios-web/fundamentos/math.js
@@ -0,0 +1,14 @@
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) //Math.pow = levado a 2 potencia
+
+function raizQadrada ( r ){
+
+    let raiz = Math.sqrt( r )
+
+    return raiz
+}
+
+console.log(raizQadrada(10).toFixed(2))
+
+console.log("O valor da area é:",area.toFixed(2))
+console.log(typeof Math)
