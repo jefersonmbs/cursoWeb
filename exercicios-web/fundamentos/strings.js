44b58d9f0635637c927f21fab1e08358312603eb
xtrapp <jefersontoga94@gmail.com>
2019-03-03 14:05:39 -0300

    Parte de Fundamentos de JS String

---
+++ exercicios-web/fundamentos/strings.js
@@ -0,0 +1,9 @@
+const escola = "Cod3r"
+
+console.log(escola.charAt(4))//Pegar a letra no indice 4 que começa em 0
+console.log(escola.charAt(5))//Ele retorna vazio caso não ache
+console.log(escola.charCodeAt(3))// Pega o valor na tabela Unicode
+console.log(escola.indexOf('C')) // Pega o valor do indice de um caracter
+
+console.log(escola.substring(1))//Mostrar a partir do indice 1
+console.log(escola.substring(0, 3))// Percorre do indie 0 ao 3 poŕem não mostra o indice 3
