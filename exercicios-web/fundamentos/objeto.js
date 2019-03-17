5a337470ffe830f5f8965a59d5e5c1453639b517
xtrapp <jefersontoga94@gmail.com>
2019-03-17 13:01:53 -0300

    Parte de Fundamentos de JS Objetos

--- exercicios-web/fundamentos/objeto.js
+++ exercicios-web/fundamentos/objeto.js
@@ -1,10 +1,15 @@
-const prod1 = {}
+const prod1 = {} // Forma de declarar um Opjeto
 prod1.nome = 'Celular Ultra Mega'
 prod1.preco = 4998.90
+prod1['Desconteo Legal'] = 0.40 // Evitar atributos com espaço
 
 console.log(prod1)
 
+
+//Criando Objeto e declarando a chave valor dentro do objeto
 const prod2 = {
     nome: 'Camisa Polo',
     preco: 70.90
 }
+
+console.log(prod2)
