442c6f1fdfc8299c80579f19ccddc0a967e19692
xtrapp <jefersontoga94@gmail.com>
2019-03-19 20:43:04 -0300

    Parte de Fundamentos de JS Usando Let 1

---
+++ exercicios-web/fundamentos/usandoLet1.js
@@ -0,0 +1,10 @@
+var numero = 1;
+{
+    let numero = 2;
+    console.log('Dentro = ', numero) //Da preferencia ao scpo melhor e depois para os maiores
+
+}
+console.log('Fora = ',numero)
+
+// Let Scopo Global , Scopo e Bloco e Scopo de Função
+// Var Scopo Gloval e Scopo de Função
