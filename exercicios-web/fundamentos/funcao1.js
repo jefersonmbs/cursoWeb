83e33135e1522efc882c14dd6d7d54a1702d573d
xtrapp <jefersontoga94@gmail.com>
2019-03-19 19:51:32 -0300

    Parte de Fundamentos de JS Função01

---
+++ exercicios-web/fundamentos/funcao1.js
@@ -0,0 +1,13 @@
+//Função é uma ação defenidas em blocos {} com as sentenças de codigos
+//Toda função tera um nome
+
+//Função sem retorno
+function imprimirSoma(a, b) {
+
+    console.log( a + b)
+
+}
+imprimirSoma(40 , 10)
+imprimirSoma(10) //NaN
+imprimirSoma(1,2,3,4,5,6,7,8,9)
+imprimirSoma()
