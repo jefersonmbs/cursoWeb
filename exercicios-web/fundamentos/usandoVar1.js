3368e962b6419aab6b1bf95aa671509d7d5b84b9
xtrapp <jefersontoga94@gmail.com>
2019-03-19 20:30:25 -0300

    Parte de Fundamentos de JS Usando Var

--- exercicios-web/fundamentos/usandoVar1.js
+++ exercicios-web/fundamentos/usandoVar1.js
@@ -7,4 +7,15 @@
         }
     }
 }
+//Por conte de ser um VAR ela sera vista fora do seu bloco
+//Por esse motivo ela é mostrada no console
 console.log(sera);
+
+//Quando se declara uma Var dentro de uma função, o scopo dessa vaviavél estará visivél só dentro da função
+function teste() {
+    var local = 123
+
+}
+//Var ou será Global ou será no scopo de função
+teste();
+console.log(local);
