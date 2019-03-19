b090f4c446315a3a1348ebc1500b7462887d3d17
xtrapp <jefersontoga94@gmail.com>
2019-03-19 20:02:35 -0300

    Parte de Fundamentos de JS Função01

---
+++ exercicios-web/fundamentos/funcao2.js
@@ -0,0 +1,20 @@
+//Armazenando uma função em uma variavel
+
+const imprimirSoma = function (a,b) {
+
+    console.log(a+b)
+}
+
+imprimirSoma(2, 4)
+
+//Armazenando uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(4,5))
+
+//Retorno implicito
+const subtracao = (a,b) => a - b
+
+console.log(subtracao(4,1))
