8941f679a4bdbdad748ce7e425f5b13c81041d87
Jeferson Martins <jeferson.santos@ilog.com.br>
2020-02-15 19:27:54 -0300

    Funçao Anonima

--- exercicios-web/função/funcaoAnonima.js
+++ exercicios-web/função/funcaoAnonima.js
@@ -11,5 +11,7 @@ const multiplicar = (x , y) => x * y;
 const imprimirResultado = ( a, b, operador = somar) => DEBUG(operador(a,b));
 
 imprimirResultado(9,2 , multiplicar);
+
 imprimirResultado(9,2,subtrair);
+
 imprimirResultado(9,2);
