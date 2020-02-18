581ff8413dfb9d4893df3bdeb4efabac32591658
Jeferson Martins <jeferson.santos@ilog.com.br>
2020-02-18 18:25:08 -0300

    Praticar

--- exercicios-web/exercicios/exercicios.js
+++ exercicios-web/exercicios/exercicios.js
@@ -124,4 +124,28 @@ const calcularrogresso = function ( historico ) {
     }
     return [records, jogoPior]
 }
-DEBUG(calcularrogresso(historicoDeJogos))
+DEBUG(calcularrogresso(historicoDeJogos));
+
+/* ​Faça uma função que receba como parâmetro um vetor de notas e mostre os conceitos de cada uma de modo que de 0,0 a 4,9
+seja atribuído o conceito D, de 5,0 a 6,9 seja atribuído o conceito C, de 7,0 a 8,9 o conceito B e de 9,0 a 10,0 o conceito A.
+ */
+let notas = [1,2,3,4,5,6,7,8,9,10,1.5,2.5,3.5,4.5,5.5,6.6,7.5,8.5,9.5,'a']
+
+const passarParaIndicador = function ( notas ) {
+    let conceito = [];
+    for(let i = 0 ; i < notas.length; i++){
+        if(notas[i]>=0 && notas[i]<=4.9){
+            conceito.push('D');
+        }else if (notas[i]> 4.9 && notas[i]<=6.9){
+            conceito.push('C');
+        }else if (notas[i] > 6.9 && notas[i] <= 8.9){
+            conceito.push('B');
+        }else if(notas[i] >=9 && notas[i]<=10){
+            conceito.push('A');
+        }else
+            conceito.push('Nota invalida')
+    }
+    return conceito
+}
+let conceito = passarParaIndicador(notas);
+DEBUG(conceito);
